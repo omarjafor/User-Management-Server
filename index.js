443f653b9b53c1eab33bdfc08937be5619d179da
omarjafor <omarjaforchy@gmail.com>
2023-10-13 00:53:53 +0600

    mongodb connection success

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,12 +11,51 @@ const user = [
     { id: 4, name: 'kbulkhan', email: 'fbulkhan@gmail.com' }
 ]
 
+app.use(cors())
+app.use(express.json());
+
+
+const uri = "mongodb+srv://omarjaforchy:CfWN2W5mNjwsKttu@cluster0.21hcnfr.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('User Management Server Running')
 })
 
 app.get('/user', (req, res) => {
     res.send(user)
+})
+
+app.post('/user', (req, res) => {
+    console.log('Post Api Hitting...')
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = user.length + 1
+    user.push(newUser);
+    res.send(newUser);
 })
 
 app.listen(port, () => {
